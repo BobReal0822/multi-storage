15051ae815f3bb42b241a6296d22fef91cda2f8a
ephoton <i@ephoton.cn>
2015-11-12 18:59:17 +0800

    use typescript classes

--- bin/lib/client-flag.js
+++ bin/lib/client-flag.js
@@ -1,51 +1,180 @@
+/**
+ *
+ * @author ephoton
+ * @link https://github.com/ephoton/client-flag
+ */
 (function () {
-    ;
     var root = typeof self == 'object' && self.self === self && self ||
         typeof global == 'object' && global.global === global && global ||
         this;
-    root.flag = 'this is a flag for test';
-    root.setFlag = function () {
-    };
-    var _defaultSetting = {};
-    var _get = function () {
-    };
-    var _set = function () {
-    };
-    var _reset = function () {
-    };
-    var _clear = function () {
-    };
-    var _setCookie = function (name, value, expires) {
-        if (!name || !value) {
-            return false;
-        }
-        else {
-            root.document.cookie = name + '=' + value + (expires ? 'expires=' + expires : null);
-            return true;
-        }
-    };
-    var _getCookie = function (name) {
-        var cookie = root.document.cookie, value = '', nameIndex, valueIndex;
-        if (cookie.length > 0) {
-            nameIndex = cookie.indexOf(name + '=');
-            if (nameIndex > -1) {
-                valueIndex = cookie.indexOf(';', nameIndex);
-                value = valueIndex > -1 ? cookie.substring(nameIndex + name.length + 1, valueIndex) : '';
+    var clientFlag = (function () {
+        function clientFlag(settings) {
+            this._config = {
+                flagName: 'flagId',
+                userData: {
+                    type: 'div',
+                    className: 'client-flag'
+                },
+                indexedDb: {
+                    dbName: 'clientFlag',
+                    tableName: 'flagTable'
+                }
+            };
+            this._defaultSettings = {
+                cookie: true,
+                localStorage: true,
+                userData: true,
+                flash: true,
+                indexedDB: true
+            };
+            for (var key in this._defaultSettings) {
+                this._setting[key] = !!settings[key] ? settings[key] : this._defaultSettings[key];
             }
         }
-        return value;
-    };
-    var _setLocalStorage = function (name, value) {
-        if (!name || !value) {
+        clientFlag.prototype.setOne = function (name, value) {
+            if (!name || !value) {
+                return false;
+            }
+            this._broadcast({ name: name, value: value }, 'setOne');
+        };
+        clientFlag.prototype.set = function (data) {
+            var dataLength = Object.keys(data).length;
+            if (dataLength == 1) {
+                this._broadcast(data, 'set');
+            }
+            else if (dataLength > 1) {
+                this._broadcast(data, 'setOne');
+            }
             return false;
+        };
+        clientFlag.prototype.get = function () {
+        };
+        clientFlag.prototype.getAll = function () {
+        };
+        clientFlag.prototype.clear = function () {
+        };
+        clientFlag.prototype._broadcast = function (data, func) {
+            for (var key in this._setting) {
+                if (this._setting[key]) {
+                    var storageClass = key[0].toUpperCase() + key.substring(1, key.length);
+                    storageClass.func.apply(this, data);
+                }
+            }
+        };
+        return clientFlag;
+    })();
+    var LocalStorage = (function () {
+        function LocalStorage(name, value) {
+            this.set(name, value);
         }
-        else {
-            root.localStorage.setItem(name, value);
-            return true;
+        LocalStorage.prototype.set = function (name, value) {
+            if (!name || !value) {
+                return false;
+            }
+            else {
+                root.localStorage.setItem(name, value);
+                return true;
+            }
+        };
+        LocalStorage.prototype.get = function (name) {
+            return name ? root.localStorage.getItem(name) : '';
+        };
+        return LocalStorage;
+    })();
+    var Cookie = (function () {
+        function Cookie(name, value, expires) {
+            this.set(name, value);
         }
-    };
-    var _getLocalStorage = function (name) {
-        return name ? root.localStorage.getItem(name) : '';
-    };
+        Cookie.prototype.set = function (name, value, expires) {
+            if (!name || !value) {
+                return false;
+            }
+            else {
+                root.document.cookie = name + '=' + value + (expires ? 'expires=' + expires : null);
+                return true;
+            }
+        };
+        Cookie.prototype.get = function (name) {
+            var cookie = root.document.cookie, value = '', nameIndex, valueIndex;
+            if (cookie.length > 0) {
+                nameIndex = cookie.indexOf(name + '=');
+                if (nameIndex > -1) {
+                    valueIndex = cookie.indexOf(';', nameIndex);
+                    value = valueIndex > -1 ? cookie.substring(nameIndex + name.length + 1, valueIndex) : '';
+                }
+            }
+            return value;
+        };
+        return Cookie;
+    })();
+    var UserData = (function () {
+        function UserData(setting) {
+            this._setting = setting;
+        }
+        UserData.prototype.set = function (name, value) {
+            if (name && value) {
+                try {
+                    var element = root.document.createElement(this._setting.elementName);
+                    element.style.visibility = "hidden";
+                    element.setAttribute("class", name);
+                    document.body.appendChild(element);
+                    element.style.behavior = "url(#default#userData)";
+                    element.setAttribute(name, value);
+                    element.save(name);
+                    return true;
+                }
+                catch (error) {
+                    console.log('setUserData error', error);
+                    return false;
+                }
+            }
+            return false;
+        };
+        UserData.prototype.get = function (name) {
+            try {
+                var element = root.document.getElementsByClassName(this._setting.className);
+                element.load(name);
+                return element.getAttribute(name) || '';
+            }
+            catch (error) {
+                console.log('error in getFlashCookie', error);
+                return '';
+            }
+        };
+        return UserData;
+    })();
+    var IndexedDB = (function () {
+        function IndexedDB(setting) {
+            this._settings = setting;
+        }
+        IndexedDB.prototype.set = function (name, value) {
+        };
+        IndexedDB.prototype.get = function (name) {
+            var localIndexedDB = root.indexedDB = root.indexedDB || root.mozIndexedDB || root.webkitIndexedDB || window.msIndexedDB;
+            var dbRequest = localIndexedDB.open(this._settings.dbName);
+            dbRequest.onerror = function (event) {
+                console.log('error in setIindexedDB:', event);
+            };
+            dbRequest.onupgradeneeded = function (event) {
+                var db = event.target.result;
+                var store = db.createObjectStore(this._settings.tableName, {
+                    keyPath: "name",
+                    unique: false
+                });
+            };
+        };
+        return IndexedDB;
+    })();
+    ;
+    var Flash = (function () {
+        function Flash(setting) {
+            this._setting = setting;
+        }
+        Flash.prototype.set = function () {
+        };
+        Flash.prototype.get = function () {
+        };
+        return Flash;
+    })();
 }());
 //# sourceMappingURL=client-flag.js.map
--- lib/client-flag.ts
+++ lib/client-flag.ts
@@ -1,106 +1,265 @@
+/**
+ * 
+ * @author ephoton
+ * @link https://github.com/ephoton/client-flag
+ */
+
 (function () {
-	interface settingInfo {
-		cookie: boolean;
-		localStorage: boolean;
-		userData: boolean;
-		flashCookie: boolean;
-        indexedDB: boolean;
-	};
-	
+
     var root = typeof self == 'object' && self.self === self && self ||
-            typeof global == 'object' && global.global === global && global ||
-            this;
-			
-    root.flag = 'this is a flag for test';
-    root.setFlag = function() {
-        
+        typeof global == 'object' && global.global === global && global ||
+        this;
+    
+    interface clientFlagInfo {
+        [key: string]: boolean;
+        cookie: boolean;
+        localStorage: boolean;
+        userData: boolean;
+        flash: boolean;
+        indexedDB: boolean;
     }
     
-    var _defaultSetting = {
-        
+    interface IndexedDBInfo {
+        dbName: string,
+        tableName: string,
     }
     
-    var _get = function() {
-        
+    interface UserDataInfo {
+        elementName: string;
+        className: string;
     }
     
-    var _set = function() {
-        
+    interface FlashCookieInfo {
+        swfPath: string;
     }
     
-    var _reset = function() {
-        
-    }
-    
-    var _clear = function() {
-        
-    }
-    
-    var _setCookie = function(name: string, value: string, expires?: Date): boolean {
-        if(!name || !value) {
-            return false;
-        }
-        else {
-            root.document.cookie = name + '=' + value + (expires ? 'expires=' + expires : null);
-            return true;
-        }
-    }
-    
-    var _getCookie = function(name: string): string {
-        var 
-            cookie = root.document.cookie,
-            value = '',
-            nameIndex: number,
-            valueIndex: number;
-            
-        if(cookie.length > 0) {
-            nameIndex = cookie.indexOf(name + '=');
-            if(nameIndex > -1) {
-                valueIndex = cookie.indexOf(';', nameIndex);
-                value = valueIndex > -1 ? cookie.substring(nameIndex + name.length + 1, valueIndex) : '';
+    class clientFlag {
+
+        _config = {
+            flagName: 'flagId',
+            userData: {
+                type: 'div',
+                className: 'client-flag'
+            },
+            indexedDb: {
+                dbName: 'clientFlag',
+                tableName: 'flagTable'
             }
         }
-        return value;
-    }
-    
-    var _setLocalStorage = function(name: string, value: string): boolean {
-        if(!name || !value) {
+        
+       _defaultSettings = <clientFlagInfo>{
+            cookie: true,
+            localStorage: true,
+            userData: true,
+            flash: true,
+            indexedDB: true
+        }
+        
+        _setting: clientFlagInfo;
+        
+        constructor(settings: clientFlagInfo) {
+            for(let key in this._defaultSettings) {
+                this._setting[key] = !!settings[key] ? settings[key] : this._defaultSettings[key];
+            }
+        }
+        
+        setOne(name: string, value: string) {
+            if(!name || !value) {
+                return false;
+            }
+            this._broadcast({name, value}, 'setOne');
+        }
+        
+        set(data: {[key: string] : any}) {
+            let dataLength = Object.keys(data).length;
+            if(dataLength == 1) {
+                this._broadcast(data, 'set');
+            }
+            else if(dataLength > 1) {
+                this._broadcast(data, 'setOne')
+            }
             return false;
         }
-        else {
-            root.localStorage.setItem(name,  value);
-            return true;
+        
+        get() {
+            
+        }
+        
+        getAll() {
+            
+        }
+        
+        clear() {
+            
+        }
+        
+        _broadcast(data: {[key: string] : any}, func: string) {
+            for(let key in this._setting) {
+                if(this._setting[key]) {
+                    let storageClass = key[0].toUpperCase() + key.substring(1, key.length);
+                    storageClass.func.apply(this, data);
+                }
+            }
+        }
+        
+    }
+
+    // class localStorage
+    class LocalStorage {
+        
+        constructor(name: string, value: string) {
+            this.set(name, value);
+        }
+        
+        set(name: string, value: string): boolean {
+            if(!name || !value) {
+                return false;
+            }
+            else {
+                root.localStorage.setItem(name,  value);
+                return true;
+            }
+        }
+        
+        get(name: string): string {
+            return name ? root.localStorage.getItem(name) : '';
+        }
+        
+    }
+    
+    class Cookie {
+        
+        constructor(name: string, value: string, expires?: Date) {
+            this.set(name, value);
+        }
+        
+        set(name: string, value: string, expires?: Date): boolean {
+            if(!name || !value) {
+                return false;
+            }
+            else {
+                root.document.cookie = name + '=' + value + (expires ? 'expires=' + expires : null);
+                return true;
+            }
+        }
+        
+        get(name: string): string {
+            var 
+                cookie = root.document.cookie,
+                value = '',
+                nameIndex: number,
+                valueIndex: number;
+                
+            if(cookie.length > 0) {
+                nameIndex = cookie.indexOf(name + '=');
+                if(nameIndex > -1) {
+                    valueIndex = cookie.indexOf(';', nameIndex);
+                    value = valueIndex > -1 ? cookie.substring(nameIndex + name.length + 1, valueIndex) : '';
+                }
+            }
+            return value;
         }
     }
     
-    var _getLocalStorage = function(name: string): string {
-        return name ? root.localStorage.getItem(name) : '';
-    }
-    
-    var _getUserData = function(name: string): string {
+    class UserData {
+        
+        _setting: UserDataInfo;
+        
+        constructor(setting: UserDataInfo) {
+            this._setting = setting;
+        }
+        
+        set(name: string, value: string): boolean {
+            if(name && value) {
+                try {
+                    var element = root.document.createElement(this._setting.elementName)
+                    element.style.visibility = "hidden";
+                    // element.style.position = "absolute";
+                    element.setAttribute("class", name);
+                    document.body.appendChild(element);
+                    element.style.behavior = "url(#default#userData)";
+                    element.setAttribute(name, value);
+                    element.save(name);
+                    return true;
+                }
+                catch(error) {
+                    console.log('setUserData error', error);
+                    return false;
+                }
+            }
+            return false;
+        }
+        
+        get(name: string): string {
+            try {
+                var element = root.document.getElementsByClassName(this._setting.className);
+                element.load(name);
+                return element.getAttribute(name) || '';
+            }
+            catch(error) {
+                console.log('error in getFlashCookie', error);
+                return '';
+            }
+        }
         
     }
     
-    var _setUserData = function(name: string, value: string): boolean {
+    class IndexedDB {
         
-    }
+        _settings: IndexedDBInfo;
+        
+        constructor(setting: IndexedDBInfo) {
+            this._settings = setting;
+        }
+        
+        set(name:string, value: string) {
+            
+        }
+        
+        get(name: string) {
+            var localIndexedDB = root.indexedDB = root.indexedDB || root.mozIndexedDB || root.webkitIndexedDB || window.msIndexedDB;
+            var dbRequest = localIndexedDB.open(this._settings.dbName);
+            dbRequest.onerror = function(event: any) {
+                console.log('error in setIindexedDB:', event);
+            }
+            dbRequest.onupgradeneeded = function(event: any) {
+                var db = event.target.result;
+                var store = db.createObjectStore(this._settings.tableName, {
+                    keyPath: "name",
+                    unique: false
+                })
     
-    var _getFlashCookie = function(name: string): string {
+            }
+            // dbRequest.onsuccess = function(event: any) {
+            //     var idb = event.target.result;
+            //     if (idb.objectStoreNames.contains("evercookie")) {
+            //         var tx = idb.transaction(["evercookie"], "readwrite");
+            //         var objst = tx.objectStore("evercookie");
+            //         var qr = objst.put({
+            //             "name": name,
+            //             "value": value
+            //         })
+            //     } idb.close();
+            // }
+        }
         
-    }
+    };
     
-    var _setFlashCookie = function(name: string, value: string): string {
+    class Flash {
         
+        _setting: FlashCookieInfo;
+        
+        constructor(setting: FlashCookieInfo) {
+            this._setting = setting;
+        }
+        
+        set() {
+            
+        }
+        
+        get() {
+            
+        }
     }
     
 }());
-
-	// var setCookieExpires = function() {
-	// 	// console.log('set cookie now');
-	// 	var now = new Date();
-	// 	var time = now.getTime();
-	// 	var interval = 30;
-	// 	var expireTime = time +  interval * 1000;
-	// 	now.setTime(expireTime);
-	// 	document.cookie = 'uidExpires='+time+';expires='+now.toGMTString()+';path=/';
-	// };
